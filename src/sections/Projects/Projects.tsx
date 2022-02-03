befcda7beece3f9f0ad2d727a1d115db2f51fbd9
Logan Kuzyk <logan@kuzyk.net>
2022-02-02 16:45:52 -0800

    Make projects data file

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,41 +1,10 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Flex, SimpleGrid } from "@chakra-ui/react";
 
-import { Project } from "../../types";
 import { ProjectCard } from "./ProjectCard";
-import sharedditMacbook from "../../assets/images/shareddit/shareddit-macbook.png";
+import { projects } from "../../assets/data/projects";
 
 export const Projects: React.FC = () => {
-  const projects: Project[] = useMemo(
-    () => [
-      {
-        title: "shareddit",
-        description:
-          "A web application to generate PNG images from reddit submissions and comment threads. First launched in Summer 2020, the project has improved greatly since its first iteration.",
-        repoName: "shareddit",
-        link: "https://shareddit.com",
-        mainTech: ["React", "TypeScript", "Express.js", "Nginx"],
-        image: sharedditMacbook,
-      },
-      {
-        title: "Gecko for reddit",
-        description:
-          "A cross-platform reddit client written in React Native with a focus on efficient and logical navigation.",
-        repoName: "gecko-for-reddit",
-        mainTech: ["React Native", "TypeScript", "React Query"],
-      },
-      {
-        title: "Discord Assignment Reminder Bot",
-        description:
-          "A Discord bot designed for the UVic Engineering & Computer Science server, to keep track of important dates for each course.",
-        mainTech: ["Node.js", "Discord.js"],
-        repoName: "discord-reminder-bot",
-      },
-    ],
-
-    []
-  );
-
   return (
     <Flex>
       <SimpleGrid columns={[1, 1, 1, 2]} spacing={6} flexWrap="wrap">
