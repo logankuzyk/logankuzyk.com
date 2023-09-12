45dbaba9a5922942e29476625e51b1c2f2327153
Logan Kuzyk <logan@kuzyk.net>
2023-09-12 14:22:32 -0700

    Catch GitHub fetch error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,14 @@ import { Navigation } from "@src/components/Navigation";
 import { NavigationProvider } from "@src/contexts/NavigationContext";
 import { db } from "@src/firebase";
 import { Repository, Project, Skill } from "@src/types";
-import { getStoredImage } from "@src/util";
+import { getStoredImage, notEmpty } from "@src/util";
 
 const fetchRepository = async (name: string): Promise<Repository> => {
   const response = await fetch(
     `https://api.github.com/repos/logankuzyk/${name}`
   );
   if (!response.ok) {
-    throw new Error("Failed to contact GitHub API");
+    throw new Error("Failed to retrieve GitHub data - " + response.statusText);
   }
   return response.json();
 };
@@ -29,26 +29,34 @@ export const getStaticProps: GetStaticProps<{
 
   const projectsSnapshot = await get(child(dbRef, "projects"));
   const projectsNoRepos = projectsSnapshot.val() as Omit<Project, "repo">[];
-  const projects = (await Promise.all(
-    projectsNoRepos.map(async (project) => {
-      const repo = await fetchRepository(project.repoName);
+  const projects = (
+    await Promise.all(
+      projectsNoRepos.map(async (project) => {
+        try {
+          const repo = await fetchRepository(project.repoName);
 
-      if (!project.image) {
-        return {
-          ...project,
-          repo,
-        };
-      } else {
-        return {
-          ...project,
-          repo,
-          image: await getStoredImage(
-            "images/projects/" + project.repoName + "/" + project.image
-          ),
-        };
-      }
-    })
-  )) as Project[];
+          if (!project.image) {
+            return {
+              ...project,
+              repo,
+            };
+          } else {
+            return {
+              ...project,
+              repo,
+              image: await getStoredImage(
+                "images/projects/" + project.repoName + "/" + project.image
+              ),
+            };
+          }
+        } catch {
+          // TODO: make repo property optional,
+          // so the projects will still be displayed if GitHub can't be reached.
+          return undefined;
+        }
+      })
+    )
+  ).filter(notEmpty) as Project[];
 
   const skillsSnapshot = await get(child(dbRef, "skills"));
   const skillsNoImages = skillsSnapshot.val() as Skill[];
