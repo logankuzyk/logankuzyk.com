9fb09b6b88a4bd04fd76c0bd4b19368e9eb2681b
Logan Kuzyk <logan@kuzyk.net>
2023-09-12 21:06:12 -0700

    Add Nord colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,27 @@ const config: Config = {
     "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // map to CSS variables
+      colors: {
+        "nord-fg0": "var(--nord-fg0)",
+        "nord-fg1": "var(--nord-fg1)",
+        "nord-fg2": "var(--nord-fg2)",
+        "nord-fg3": "var(--nord-fg3)",
+        "nord-bg0": "var(--nord-bg0)",
+        "nord-bg1": "var(--nord-bg1)",
+        "nord-bg2": "var(--nord-bg2)",
+        "nord-teal": "var(--nord-teal)",
+        "nord-aqua": "var(--nord-aqua)",
+        "nord-blue-light": "var(--nord-blue-light)",
+        "nord-blue-dark": "var(--nord-blue-dark)",
+        "nord-red": "var(--nord-red)",
+        "nord-orange": "var(--nord-orange)",
+        "nord-yellow": "var(--nord-yellow)",
+        "nord-green": "var(--nord-green)",
+        "nord-purple": "var(--nord-purple)",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
